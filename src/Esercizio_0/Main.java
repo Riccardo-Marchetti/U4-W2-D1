0b4c4f2c037e55d4a1d06e25e484055c526be4e0
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-03-11 17:06:34 +0100

    prove es.0

--- src/Esercizio_0/Main.java
+++ src/Esercizio_0/Main.java
@@ -2,6 +2,12 @@ package Esercizio_0;
 
 public class Main {
     public static void main(String[] args) {
+        int[] array = new int[2];
+        array[3] = 3;
+        int num1 = 3;
+        int num2 = 4;
+        int somma = num1 / num2;
 
     }
+
 }
